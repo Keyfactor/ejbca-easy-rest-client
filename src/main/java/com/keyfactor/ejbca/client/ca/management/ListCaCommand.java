29eb65b588c19560ee08d6270c6735bdad4f59f8
Mike Agrenius Kushner <mike.kushner@keyfactor.com>
2024-11-12 10:10:12 +0100

    feat: First iteration of the listcacommand

--- src/main/java/com/keyfactor/ejbca/client/ca/management/ListCaCommand.java
+++ src/main/java/com/keyfactor/ejbca/client/ca/management/ListCaCommand.java
@@ -10,7 +10,7 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
-package com.keyfactor.ejbca.client;
+package com.keyfactor.ejbca.client.ca.management;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +20,9 @@ import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -27,24 +30,32 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.log4j.Logger;
 import org.ejbca.ui.cli.infrastructure.command.CommandResult;
 import org.ejbca.ui.cli.infrastructure.parameter.ParameterContainer;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.keyfactor.util.CertTools;
+
 /**
- * A CLI command for retrieving the status o the EJBCA REST API.
- *
+ * Lists all available CAs
  */
-public class GetStatusCommand extends ErceCommandBase {
 
+public class ListCaCommand extends CaCommandBase {
 
-	private static final String COMMAND_URL= "/ejbca/ejbca-rest-api/v2/certificate/status";
+	private static final String COMMAND_URL= "/ejbca/ejbca-rest-api/v1/ca";
 
-	private static final Logger log = Logger.getLogger(GetStatusCommand.class);
-
-
+	private static final Logger log = Logger.getLogger(ListCaCommand.class);
+	
+	private static final String ISSUER_DN_LABEL = "issuer_dn";
+	private static final String IS_EXTERNAL_LABEL = "external";
+	private static final String SUBJECT_DN_LABEL = "subject_dn";
+	private static final String NAME_LABEL = "name";
+	private static final String ID_LABEL = "id";
+	private static final String EXPIRES_LABEL = "expiration_date";
+	
 	@Override
-	protected CommandResult execute(ParameterContainer parameters) {	
+	protected CommandResult execute(ParameterContainer parameters) {
 		final String restUrl = new StringBuilder().append("https://").append(getHostname()).append(COMMAND_URL).toString();
 		try {
 			// Construct the parameter payload
@@ -61,14 +72,27 @@ public class GetStatusCommand extends ErceCommandBase {
 					break;
 				case 200:
 				case 201:
-					final JSONParser jsonParser = new JSONParser();
-					final JSONObject actualJsonObject = (JSONObject) jsonParser.parse(responseString);
-					final String status = (String) actualJsonObject.get("status");
-					final String version = (String) actualJsonObject.get("version");
-					final String revision = (String) actualJsonObject.get("revision");
-					log.info("Current Status of this instance of EJBCA is: " + status);
-					log.info("Current REST API Version: " + version);
-					log.info("Current version of EJBCA is: " + revision);					
+					final JSONParser jsonParser = new JSONParser();					
+					final JSONArray jsonArray = (JSONArray) jsonParser.parse(responseString);
+					@SuppressWarnings("unchecked")
+		            Iterator<JSONObject> iterator = jsonArray.iterator();
+					StringBuilder stringBuilder = new StringBuilder();
+					List<String[]> caContents = new ArrayList<>();
+		            while (iterator.hasNext()) {
+		            	JSONObject jsonObject = iterator.next();
+		            	final String issuerDn = (String) jsonObject.get(ISSUER_DN_LABEL);
+		            	final String subjectDn = (String) jsonObject.get(SUBJECT_DN_LABEL);
+		            	final String caId = (String) jsonObject.get(ID_LABEL);
+		            	final String name = (String) jsonObject.get(NAME_LABEL);
+		            	final String expires = (String) jsonObject.get(EXPIRES_LABEL);
+		            	caContents.add(new String[]{name, caId, subjectDn, issuerDn, expires});
+
+		            }
+		            stringBuilder.append(bold("The following CAs are available in the current instance:\n"));
+		            stringBuilder.append(formatTable(1, new String[] { "Name:", "CA ID:", "Subject DN:", "Issuer DN:", "Expires:" }, caContents));
+		            stringBuilder.append("\n");
+					log.info(stringBuilder.toString());
+					
 					break;
 				default:
 					log.error("Return code was: " + response.getStatusLine().getStatusCode() + ": " + responseString);
@@ -85,15 +109,15 @@ public class GetStatusCommand extends ErceCommandBase {
 
 		return CommandResult.SUCCESS;
 	}
-
+	
 	@Override
 	public String getMainCommand() {
-		return "status";
+		return "listcas";
 	}
-
+	
 	@Override
 	public String getCommandDescription() {
-		return "Command for getting the status of a resource";
+		return "Lists all available CAs";
 	}
 
 	@Override
